4a62c305ec6964701b2e4a2df368b76c4912872d
Peter <undevil89@gmail.com>
2022-10-19 18:56:05 +0300

    Second test of commitment

--- src/Main.java
+++ src/Main.java
@@ -14,50 +14,47 @@ public class Main {
     public static final String DECODE = "decode";
     public static final String BRUTE_FORCE = "bruteForce";
 
-    public static void main(String[] args) throws NumberFormatException {
+    public static void main(String[] args) {
         String operation = args[0];
         String inputFilePath = args[1];
-        String key = args[2]; //Integer.parseInt
-        String content = null;
+        String key = args[2];
+
+        String fileContent = null;
 
         if (!Files.isRegularFile(Path.of(inputFilePath))) {
             throw new IllegalArgumentException("please, provide correct input file path");
         }
 
         if (ENCODE.equalsIgnoreCase(operation)) {
-            String encodedFilePath = rename(inputFilePath, "encode");              //(filePath.substring(0, filePath.lastIndexOf('.')) + "_encoded.txt");
-            content = reader(inputFilePath);
+            fileContent = reader(inputFilePath);
 
-            Encoder enc = new Encoder(content, Integer.parseInt(key));
+            Encoder enc = new Encoder(fileContent, Integer.parseInt(key));
 
+            String encodedFilePath = rename(inputFilePath, "encode");
             writer(encodedFilePath, enc.getEncText());
 
-
         } else if (DECODE.equalsIgnoreCase(operation)) {
-            String decodedFilePath = rename(inputFilePath, "decode");                //(filePath.substring(0, filePath.lastIndexOf('_')) + "_decoded.txt");
-            content = reader(inputFilePath);
+            fileContent = reader(inputFilePath);
 
-            Decoder dec = new Decoder(content, Integer.parseInt(key));
+            Decoder dec = new Decoder(fileContent, Integer.parseInt(key));
 
+            String decodedFilePath = rename(inputFilePath, "decode");
             writer(decodedFilePath, dec.getDecText());
 
-
         } else if (BRUTE_FORCE.equalsIgnoreCase(operation)) {
-
             StaticAnalysis staticAnalysis = new StaticAnalysis();
-            content = reader(inputFilePath);
+            fileContent = reader(inputFilePath);
             String textToCompare = reader(key);
 
-            int bruteForceKey = (staticAnalysis.bruteForce(content, textToCompare));
+            int bruteForceKey = (staticAnalysis.bruteForce(fileContent, textToCompare));
 
             System.out.println(bruteForceKey);
 
-            Decoder dec = new Decoder(content, bruteForceKey);
+            Decoder dec = new Decoder(fileContent, bruteForceKey);
 
             String decodedFilePath = rename(inputFilePath, "bruteForce", bruteForceKey);
 
             writer(decodedFilePath, dec.getDecText());
-
 
         }
 
